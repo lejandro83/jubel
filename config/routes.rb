6bc425f6927f0bfe8a12e443a8de48175b63946a
Alejandro Sandoval <alejandro@Alejandros.local>
2017-01-05 10:27:34 -0600

    Adds git ignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+require 'api_constraints'
+
+Jubel::Application.routes.draw do
+
+  devise_for :users
+	namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 end
